c088c9df07685ffa80ac09076c0dcd831a6ceb23
dana <dana_s@maximail.org>
2025-01-22 20:41:51 +0500

    fix: bundle is clear in the beginning of executing in 5 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const fs = require('fs');
 
 const styleFolderPath = path.join(__dirname, 'styles');
-const buildFile = path.join(__dirname, 'project-dist', 'bundle.css');
+const projectFolder = path.join(__dirname, 'project-dist');
+const buildFile = path.join(projectFolder, 'bundle.css');
 
 async function logChunks(readable) {
   let result = '';
@@ -21,25 +22,42 @@ function writeFile(data) {
   });
 }
 
-fsPromises
-  .readdir(styleFolderPath, { withFileTypes: true })
-  .then((files) => {
-    return files.filter(
-      (file) => file.isFile() && path.extname(file.name) === '.css',
-    );
-  })
-  .then((files) => {
+function deleteOldFiles() {
+  return fsPromises.readdir(projectFolder).then((files) => {
     files.forEach((file) => {
-      const readStream = fs.createReadStream(
-        styleFolderPath + `/${file.name}`,
-        'utf-8',
-      );
-      const str = logChunks(readStream);
-
-      str.then((res) => {
-        readStream.close();
-        writeFile(res).then(() => readStream.read());
-      });
+      if (file.includes('bundle')) {
+        fs.unlink(buildFile, (err) => {
+          if (err) throw err;
+        });
+      }
     });
+  });
+}
+
+deleteOldFiles()
+  .then(() => {
+    fsPromises
+      .readdir(styleFolderPath, { withFileTypes: true })
+      .then((files) => {
+        return files.filter(
+          (file) => file.isFile() && path.extname(file.name) === '.css',
+        );
+      })
+      .then((files) => {
+        files.forEach((file) => {
+          const readStream = fs.createReadStream(
+            styleFolderPath + `/${file.name}`,
+            'utf-8',
+          );
+          const str = logChunks(readStream);
+
+          str.then((res) => {
+            readStream.close();
+            writeFile(res).then(() => readStream.read());
+          });
+        });
+      })
+      .catch((err) => console.log(err));
   })
-  .catch((err) => console.log(err));
+
+
